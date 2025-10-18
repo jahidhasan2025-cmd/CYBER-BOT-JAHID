23cfe23ae40b900c265958895ac12fa10469c516
JAHID HASAN <jahidmariya63@gmail.com>
2025-10-18 13:27:12 +0600

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -1,1 +1,28 @@
+const chalk = require('chalk');
+module.exports = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.bold.hex("#00FF00").bold('[ Error ] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.bold.hex("#00FF00").bold('[ Error ] »') + data);
+     break;
+		default:			        
+                        console.log(chalk.bold.hex("#00FF00").bold(`${option} » `) + data);
+			break;
+	}
+}
 
+module.exports.loader = (data, option) => {
+	switch (option) {
+		case "warn":
+			console.log(chalk.bold.hex("#00FF00").bold('[ JR-JAHID ] » ') + data);
+			break;
+		case "error":
+			console.log(chalk.bold.hex("#00FF00").bold('[ JR-JAHID ] » ') + data);
+			break;
+		default:
+			console.log(chalk.bold.hex("#00FF00").bold(`[ JR-JAHID ] » `) + data);
+			break;
+	}
+}
